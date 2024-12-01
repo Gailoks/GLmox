0b60fe169eb1da2d1c8d46e2db2d13078a3a4aa8
gailoks <yra.cimonov@gmail.com>
2024-12-01 16:05:24 +0000

    fixes again

--- setup..sh
+++ setup..sh
@@ -49,11 +49,11 @@ cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
 
 # Setup time zone
 echo Select a time zone
-echo $(ls  -l user/share/zoneinfo)
+echo $(ls  -l usr/share/zoneinfo)
 read region
-echo $(ls -l user/share/zoneinfo/$region)
+echo $(ls -l usr/share/zoneinfo/$region)
 read country
-ln -sf usr/share/zoneinfo/$region/$country /etc/localtime
+ln -sf usr/share/zoneinfo/$region/$country etc/localtime
 
 # Gen fstab file
 genfstab -U /mnt/gentoo > etc/fstab
